9b63eba6b85036a46bba1baab5a874b6f970fcfa
Tabish Shaikh <tabish@archive.org>
2021-09-17 20:05:32 +0530

    feat: custom tests added for deposit and withdraw and strategy resolvers updated

--- config/StrategyResolver.py
+++ config/StrategyResolver.py
@@ -11,34 +11,45 @@ class StrategyResolver(StrategyCoreResolver):
         (Strategy Must Implement)
         """
         # E.G
-        # strategy = self.manager.strategy
-        # return {
-        #     "gauge": strategy.gauge(),
-        #     "mintr": strategy.mintr(),
-        # }
-
-        return {}
+        strategy = self.manager.strategy
+        return {
+            "vault": strategy.VAULT(),
+            "pool": strategy.want(),
+            "lpcomponent": strategy.lpComponent(),
+        }
 
     def hook_after_confirm_withdraw(self, before, after, params):
         """
         Specifies extra check for ordinary operation on withdrawal
         Use this to verify that balances in the get_strategy_destinations are properly set
         """
-        assert False
+        # After withdraw from strategy the lpcomponent should decrease as it will be burned ...
+        assert after.balances("want", "lpcomponent") <= before.balances(
+            "want", "lpcomponent"
+        )
+
+        # After withdraw from strategy vault balance should decrease ...
+        assert after.balances("want", "vault") < before.balances("want", "vault")
 
     def hook_after_confirm_deposit(self, before, after, params):
         """
         Specifies extra check for ordinary operation on deposit
         Use this to verify that balances in the get_strategy_destinations are properly set
         """
-        assert False
+        # Deposit from deployer to sett therefore this check is not needed
+        assert True
 
     def hook_after_earn(self, before, after, params):
         """
         Specifies extra check for ordinary operation on earn
         Use this to verify that balances in the get_strategy_destinations are properly set
         """
-        assert False
+        # Earn deposits the amount from sett to Strategy therefore check if we have got lpcomponent after depositing in the pool
+        assert after.balances("want", "lpcomponent") > before.balances(
+            "want", "lpcomponent"
+        )
+        # the balance of vault should increase after deposit
+        assert after.balances("want", "vault") > before.balances("want", "vault")
 
     def confirm_harvest(self, before, after, tx):
         """
--- tests/test_custom.py
+++ tests/test_custom.py
@@ -11,5 +11,80 @@ from helpers.time import days
 """
 
 
-def test_my_custom_test(deployed):
-    assert False
+def test_custom_deposit(deployer, sett, strategy, want):
+    # Setup
+    startingBalance = want.balanceOf(deployer)
+
+    depositAmount = startingBalance // 2
+    assert startingBalance >= depositAmount
+    assert startingBalance >= 0
+    # End Setup
+    print("Setup Complete")
+
+    # Deposit
+    assert want.balanceOf(sett) == 0
+
+    want.approve(sett, MaxUint256, {"from": deployer})
+    sett.deposit(depositAmount, {"from": deployer})
+
+    available = sett.available()
+    assert available > 0
+    print("Avaiable amount in sett: ", sett.available())
+
+    sett.earn({"from": deployer})
+
+    amountLPComponent = strategy.balanceOfPool()
+
+    print("Amount of LPComponent(BAL-WBTC-WETH-USDC-Token):", amountLPComponent)
+
+    assert amountLPComponent > 0
+
+    # Will not work currently as the Pool Contract is not verified on arbitrum ... and brownie throws an error
+    # ValueError: Failed to retrieve data from API: {'status': '0', 'message': 'NOTOK', 'result': 'Contract source code not verified'}
+    # balanceOfPoolInWant = (
+    #     strategy.balanceOfPoolinWant()
+    # )  # Getting the balance of pool in terms of want
+
+    # assert balanceOfPoolInWant > 0
+
+
+def test_custom_withdraw_all(deployer, sett, strategy, want, controller):
+    # Setup
+    startingBalance = want.balanceOf(deployer)
+
+    depositAmount = startingBalance // 2
+    assert startingBalance >= depositAmount
+    assert startingBalance >= 0
+    # End Setup
+    print("Setup Complete")
+
+    # Deposit
+    assert want.balanceOf(sett) == 0
+
+    want.approve(sett, MaxUint256, {"from": deployer})
+    sett.deposit(depositAmount, {"from": deployer})
+
+    available = sett.available()
+    assert available > 0
+    print("Avaiable amount in sett: ", sett.available())
+
+    sett.earn({"from": deployer})
+
+    amountLPComponent = strategy.balanceOfPool()
+
+    print("Amount of LPComponent(BAL-WBTC-WETH-USDC-Token):", amountLPComponent)
+
+    assert amountLPComponent > 0
+
+    # Deposit complete
+
+    # Withdraw
+    controller.withdrawAll(strategy.want(), {"from": deployer})
+
+    assert (
+        strategy.balanceOfPool() == 0
+    )  # After withdrawAll no LPComponent should be there
+
+    assert (
+        strategy.totalDepositedinPool() == 0
+    )  # totalDepositedinPool balance should be zero
