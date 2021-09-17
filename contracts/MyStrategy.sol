df83b22887d750ae991a854b5685337159451675
Tabish Shaikh <tabish@archive.org>
2021-09-17 20:04:39 +0530

    feat: deposit, withdraw, withdrawAll functionality added

--- contracts/MyStrategy.sol
+++ contracts/MyStrategy.sol
@@ -8,9 +8,11 @@ import "../deps/@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol
 import "../deps/@openzeppelin/contracts-upgradeable/math/MathUpgradeable.sol";
 import "../deps/@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
 import "../deps/@openzeppelin/contracts-upgradeable/token/ERC20/SafeERC20Upgradeable.sol";
+import "../interfaces/erc20/IERC20.sol";
 
 import "../interfaces/badger/IController.sol";
 import {IVault} from "../interfaces/balancer/IVault.sol";
+import {IBalancerPool} from "../interfaces/balancer/IBalancerPool.sol";
 import "../interfaces/balancer/IAsset.sol";
 
 import {BaseStrategy} from "../deps/BaseStrategy.sol";
@@ -23,15 +25,28 @@ contract MyStrategy is BaseStrategy {
     address public lpComponent; // Token we provide liquidity with
     address public reward; // Token we farm and swap to want / lpComponent
 
-    uint256 totalDepositedinPool = 0; // tracking total wbtc deposited in the pool
+    uint256 public totalDepositedinPool; // tracking total wbtc deposited in the pool
 
     address public constant VAULT = 0xBA12222222228d8Ba445958a75a0704d566BF2C8; // Balancer Vault address on Arbitrum
+    address public constant HELPER = 0x77d46184d22CA6a3726a2F500c776767b6A3d6Ab; // Balancer Helper Contract Address on Arbitrum - https://github.com/balancer-labs/balancer-v2-monorepo/blob/18bd5fb5d87b451cc27fbd30b276d1fb2987b529/pkg/standalone-utils/contracts/BalancerHelpers.sol
     bytes32 public constant poolId =
         0x64541216bafffeec8ea535bb71fbc927831d0595000100000000000000000002; // Pool Id of WBTC/WETH/USDC Balancer Pool on Arbitrum
 
-    address public constant WBTC = 0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f; // WBTC on Arbitrum
+    // Found using:
+    // >>> vault.getPool(0x64541216bafffeec8ea535bb71fbc927831d0595000100000000000000000002)
+    // ("0x64541216bAFFFEec8ea535BB71Fbc927831d0595", 1)
+    IBalancerPool public bpt =
+        IBalancerPool(0x64541216bAFFFEec8ea535BB71Fbc927831d0595);
+
+    // address public constant WBTC = 0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f; // WBTC on Arbitrum
     address public constant WETH = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1; // WETH on Arbitrum
     address public constant USDC = 0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8; // USDC on Arbitrum
+
+    address public constant SUSHISWAP_ROUTER =
+        0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506; // SushiSwap router on arbitrum
+
+    uint256 public slippage;
+    uint256 public constant MAX_BPS = 10000;
 
     // Used to signal to the Badger Tree that rewards where sent to it
     event TreeDistribution(
@@ -63,12 +78,19 @@ contract MyStrategy is BaseStrategy {
         lpComponent = _wantConfig[1];
         reward = _wantConfig[2];
 
+        totalDepositedinPool = 0; // Setting total amount deposit in balancer vault as 0
+        slippage = 100; // 1% slippage allowance
+
         performanceFeeGovernance = _feeConfig[0];
         performanceFeeStrategist = _feeConfig[1];
         withdrawalFee = _feeConfig[2];
 
         /// @dev do one off approvals here
-        // IERC20Upgradeable(want).safeApprove(gauge, type(uint256).max);
+        IERC20Upgradeable(want).safeApprove(VAULT, type(uint256).max);
+        IERC20Upgradeable(reward).safeApprove(
+            SUSHISWAP_ROUTER,
+            type(uint256).max
+        );
     }
 
     /// ===== View Functions =====
@@ -83,14 +105,60 @@ contract MyStrategy is BaseStrategy {
         return "1.0";
     }
 
-    /// @dev Balance of want currently held in strategy positions
+    /// @dev Creates the Swap Request Structure required by IBalancerPool.onSwap function
+    function _getSwapRequest(
+        IERC20 token,
+        uint256 amount,
+        uint256 lastChangeBlock
+    ) public view returns (IBalancerPool.SwapRequest memory request) {
+        return
+            IBalancerPool.SwapRequest(
+                IBalancerPool.SwapKind.GIVEN_IN,
+                token,
+                IERC20(want),
+                amount,
+                poolId,
+                lastChangeBlock,
+                address(this),
+                address(this),
+                abi.encode(0)
+            );
+    }
+
+    /// @dev Balance of lpcomponent
     function balanceOfPool() public view override returns (uint256) {
         return IERC20Upgradeable(lpComponent).balanceOf(address(this));
     }
 
+    /// @dev Balance of pool currently held in strategy positions in terms of want.
+    function balanceOfPoolinWant() public returns (uint256) {
+        uint256 totalWantPooled;
+        (
+            IERC20[] memory tokens,
+            uint256[] memory totalBalances,
+            uint256 lastChangeBlock
+        ) = IVault(VAULT).getPoolTokens(poolId);
+        for (uint8 i = 0; i < 3; i++) {
+            uint256 tokenPooled =
+                totalBalances[i].mul(balanceOfPool()).div(bpt.totalSupply());
+            if (tokenPooled > 0) {
+                IERC20 token = tokens[i];
+                if (token != IERC20(want)) {
+                    IBalancerPool.SwapRequest memory request =
+                        _getSwapRequest(token, tokenPooled, lastChangeBlock);
+                    // now denomated in want
+                    // onSwap gives us the amount that would be returned if the token was to be swapped
+                    tokenPooled = bpt.onSwap(request, totalBalances, i, 0); // 0 is tokenIndex of want(WBTC)
+                }
+                totalWantPooled += tokenPooled;
+            }
+        }
+        return totalWantPooled;
+    }
+
     /// @dev Returns true if this strategy requires tending
     function isTendable() public view override returns (bool) {
-        // Checks balance of want token (WBTC) and returns true if balance of want > 0
+        // Checks balance of want token (WBTC) and returns true if balance of want > 0 as we have wbtc to deposit into the strat
         return balanceOfWant() > 0;
     }
 
@@ -108,10 +176,13 @@ contract MyStrategy is BaseStrategy {
         return protectedTokens;
     }
 
-    /// ===== Permissioned Actions: Governance =====
-    /// @notice Delete if you don't need!
-    function setKeepReward(uint256 _setKeepReward) external {
-        _onlyGovernance();
+    /// @notice sets slippage tolerance for liquidity provision in terms of BPS ie.
+    /// @notice minSlippage = 0
+    /// @notice maxSlippage = 10_000
+    function setSlippageTolerance(uint256 _s) external whenNotPaused {
+        _onlyGovernanceOrStrategist();
+        require(_s <= 10_000, "slippage out of bounds");
+        slippage = _s;
     }
 
     /// ===== Internal Core Implementations =====
@@ -133,7 +204,7 @@ contract MyStrategy is BaseStrategy {
     /// @notice Just get the current balance and then invest accordingly
     function _deposit(uint256 _amount) internal override {
         IAsset[] memory assets = new IAsset[](3);
-        assets[0] = IAsset(WBTC);
+        assets[0] = IAsset(want);
         assets[1] = IAsset(WETH);
         assets[2] = IAsset(USDC);
 
@@ -149,43 +220,64 @@ contract MyStrategy is BaseStrategy {
 
         IVault(VAULT).joinPool(poolId, address(this), address(this), request);
 
-        totalDepositedinPool.add(_amount);
+        totalDepositedinPool = totalDepositedinPool.add(_amount);
     }
 
     function _exitPosition(uint256 _wbtcAmountOut, uint256 _bptAmountIn)
         internal
     {
         IAsset[] memory assets = new IAsset[](3);
-        assets[0] = IAsset(WBTC);
+        assets[0] = IAsset(want);
         assets[1] = IAsset(WETH);
         assets[2] = IAsset(USDC);
 
-        uint256 bptAmountIn = balanceOfPool();
-
-        uint256[] memory minAmountsOut = new uint256[](3); // minAmountsOut
-        minAmountsOut[0] = _wbtcAmountOut;
-        minAmountsOut[1] = 0;
-        minAmountsOut[2] = 0;
+        uint256[] memory minAmountsOut = new uint256[](3); // minAmountsOut for respective assets
+        minAmountsOut[0] = _wbtcAmountOut.mul(slippage).div(MAX_BPS);
 
         uint256 exitTokenIndex = 0; // As wbtc is the token we want to exit with therefore exitTokenIndex = 0
 
         bytes memory userData =
             abi.encode(uint256(0), _bptAmountIn, exitTokenIndex); // Here 0: EXACT_BPT_IN_FOR_ONE_TOKEN_OUT
 
-        IVault.ExitPoolRequest memory request =
+        IVault.ExitPoolRequest memory exit_request =
             IVault.ExitPoolRequest(assets, minAmountsOut, userData, false);
 
         IVault(VAULT).exitPool(
             poolId,
             address(this),
             payable(address(this)),
-            request
+            exit_request
         );
     }
 
     /// @dev utility function to withdraw everything for migration
     function _withdrawAll() internal override {
-        _exitPosition(totalDepositedinPool, balanceOfPool());
+        uint256 _bptAmountIn = balanceOfPool();
+        uint256 _wbtcAmountOut = totalDepositedinPool;
+
+        IAsset[] memory assets = new IAsset[](3);
+        assets[0] = IAsset(want);
+        assets[1] = IAsset(WETH);
+        assets[2] = IAsset(USDC);
+
+        uint256[] memory minAmountsOut = new uint256[](3); // minAmountsOut for respective assets
+        minAmountsOut[0] = _wbtcAmountOut.mul(slippage).div(MAX_BPS);
+
+        uint256 exitTokenIndex = 0; // As wbtc is the token we want to exit with therefore exitTokenIndex = 0
+
+        bytes memory userData =
+            abi.encode(uint256(0), _bptAmountIn, exitTokenIndex); // Here 0: EXACT_BPT_IN_FOR_ONE_TOKEN_OUT
+
+        IVault.ExitPoolRequest memory exit_request =
+            IVault.ExitPoolRequest(assets, minAmountsOut, userData, false);
+
+        IVault(VAULT).exitPool(
+            poolId,
+            address(this),
+            payable(address(this)),
+            exit_request
+        );
+
         totalDepositedinPool = 0;
     }
 
@@ -204,6 +296,12 @@ contract MyStrategy is BaseStrategy {
         totalDepositedinPool = totalDepositedinPool.sub(_amount);
     }
 
+    /// @dev Swap Rewards to want token
+    function _swapRewards() internal {
+        uint256 balanceOfReward =
+            IERC20Upgradeable(reward).balanceOf(address(this));
+    }
+
     /// @dev Harvest from strategy mechanics, realizing increase in underlying position
     function harvest() external whenNotPaused returns (uint256 harvested) {
         _onlyAuthorizedActors();
@@ -211,6 +309,9 @@ contract MyStrategy is BaseStrategy {
         uint256 _before = IERC20Upgradeable(want).balanceOf(address(this));
 
         // Write your code here
+
+        // harvest will get the rewards using merkle tree proof
+        // BAL rewards will be swapped to wbtc
 
         uint256 earned =
             IERC20Upgradeable(want).balanceOf(address(this)).sub(_before);
